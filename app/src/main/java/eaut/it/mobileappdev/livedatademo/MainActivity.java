890abb6b794bfb8dc237bf9261a39905a57abb72
luanp-eaut <luanp@eaut.edu.vn>
2024-10-31 11:33:14 +0700

    first commit

---
+++ app/src/main/java/eaut/it/mobileappdev/livedatademo/MainActivity.java
@@ -0,0 +1,45 @@
+package eaut.it.mobileappdev.livedatademo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import eaut.it.mobileappdev.livedatademo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyViewModel viewModel;
+    ActivityMainBinding mainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mainBinding = DataBindingUtil.setContentView(this,
+                R.layout.activity_main);
+
+
+        viewModel = new ViewModelProvider(this)
+                .get(MyViewModel.class);
+
+        // Link the Databinding with ViewModel
+        mainBinding.setMyviewmodel(viewModel);
+
+        // Observing the LiveData
+        viewModel.getCounter().observe(this,
+                new Observer<Integer>() {
+                    @Override
+                    public void onChanged(Integer counter) {
+                        // Update the UI when the LiveData Changes
+                        mainBinding.textView2.setText(""+counter);
+                    }
+                });
+
+
+    }
+
+
+}
